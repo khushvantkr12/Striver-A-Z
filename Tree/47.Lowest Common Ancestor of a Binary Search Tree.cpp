afdae0601371cd2cbc636ba769b592cf937825f6
khushvantkr12 <khushvantkumar132003@gmail.com>
2024-08-15 00:16:47 +0530

    second

---
+++ Tree/47.Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
+        //base
+        if(root == NULL){
+            return NULL;
+        }
+
+        if(root->val > p->val && root->val > q->val){
+            return solve(root->left, p, q);
+        } else if(root->val < p->val && root->val < q->val){
+          return  solve(root->right, p, q);
+        } else if(root->val>=p->val && root->val<=q->val){
+            return root;
+        }else if(root->val<=p->val && root->val>=q->val){
+            return root;
+        }
+        return 0;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return solve(root, p, q);
+    }
+};
